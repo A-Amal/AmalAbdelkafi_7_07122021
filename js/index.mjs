a11b217d2baf683200060dfd3443b3dad1ccf66f
Abdelkafi Amal <zaghdanamal@yahoo.fr>
2022-01-06 10:20:08 +0100

    modif css

--- js/index.mjs
+++ js/index.mjs
@@ -37,7 +37,7 @@ function renderRecipes(tab) {
 }
 
 function filterSearch(term, recipes) {
-    //const t0 = performance.now(), q = recipes.length;
+    const t0 = performance.now(), q = recipes.length;
      const filtered = [];
         for (let i = 0; i < recipes.length; i++) {
             const recipe = recipes[i];
@@ -67,7 +67,7 @@ function filterSearch(term, recipes) {
                 }
             }
         }
-        //const t1 = performance.now(); console.log(`Search time ${t1 - t0} ms | ${(t1 - t0) / q} per recipe`);
+        const t1 = performance.now(); console.log(`Search time ${t1 - t0} ms | ${(t1 - t0) / q} per recipe`);
         return filtered;
 }
 
